fd5ee80829299aa33dd58392ed6a6f264a443a15
DJUR-DJUR <djurodjur@gmail.com>
2022-12-03 00:37:45 +0200

    test

--- scripts/setenv.ts
+++ scripts/setenv.ts
@@ -1,20 +1,24 @@
 const { writeFile } = require('fs');
 const { argv } = require('yargs');
+
 // read environment variables from .env file
 require('dotenv').config();
-console.log(process.env) // remove this after you've confirmed it is working
+
 // read the command line arguments passed with yargs
 const environment = argv.environment;
 const isProduction = environment === 'prod';
-const targetPath = isProduction
-   ? `./src/environments/environment.prod.ts`
-   : `./src/environments/environment.ts`;
-// we have access to our environment variables
-// in the process.env object thanks to dotenv
-if (!process.env["API_URL"] || !process.env["API_KEY"]) {
+
+if (!process.env["API_KEY"] || !process.env["API_URL"]) {
   console.error('All the required environment variables were not provided!');
   process.exit(-1);
 }
+
+const targetPath = isProduction
+   ? `./src/environments/environment.prod.ts`
+   : `./src/environments/environment.ts`;
+
+// we have access to our environment variables
+// in the process.env object thanks to dotenv
 const environmentFileContent = `
 export const environment = {
    production: ${isProduction},
@@ -22,10 +26,12 @@ export const environment = {
    API_KEY: "${process.env["API_KEY"]}"
 };
 `;
+
 // write the content to the respective file
 writeFile(targetPath, environmentFileContent, function (err: any) {
    if (err) {
       console.log(err);
    }
+
    console.log(`Wrote variables to ${targetPath}`);
 });
