0ab15d97b7c699ee95d02bc1f210326b14dbb6d5
DJUR-DJUR <djurodjur@gmail.com>
2022-12-03 00:22:16 +0200

    test

--- scripts/setenv.ts
+++ scripts/setenv.ts
@@ -2,6 +2,7 @@ const { writeFile } = require('fs');
 const { argv } = require('yargs');
 // read environment variables from .env file
 require('dotenv').config();
+console.log(process.env) // remove this after you've confirmed it is working
 // read the command line arguments passed with yargs
 const environment = argv.environment;
 const isProduction = environment === 'prod';
