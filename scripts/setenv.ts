fc39eb1e206c9013e2d07c1c6ffdcaf518b60c24
DJUR-DJUR <djurodjur@gmail.com>
2022-12-03 00:10:07 +0200

    set script

--- scripts/setenv.ts
+++ scripts/setenv.ts
@@ -5,16 +5,15 @@ require('dotenv').config();
 // read the command line arguments passed with yargs
 const environment = argv.environment;
 const isProduction = environment === 'prod';
-// read the command line arguments passed with yargs
-if (!process.env["API_URL"] || !process.env["API_KEY"]) {
-     console.error('All the required environment variables were not provided!');
-     process.exit(-1);
-  }
 const targetPath = isProduction
    ? `./src/environments/environment.prod.ts`
    : `./src/environments/environment.ts`;
 // we have access to our environment variables
 // in the process.env object thanks to dotenv
+if (!process.env["API_URL"] || !process.env["API_KEY"]) {
+  console.error('All the required environment variables were not provided!');
+  process.exit(-1);
+}
 const environmentFileContent = `
 export const environment = {
    production: ${isProduction},
