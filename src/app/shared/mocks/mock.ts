9f02cd3afc0d99fb85aeb604140a207240d7e5e9
DJUR-DJUR <djurodjur@gmail.com>
2022-12-05 00:19:48 +0200

    changed favicon

--- src/app/shared/mocks/mock.ts
+++ src/app/shared/mocks/mock.ts
@@ -1,76 +1,76 @@
 import { WeatherForDay } from "../interfaces/interfaces";
 
-export const mockWeatherForDay = undefined
-// [
-//   {
-//     date: '2020-07-10 15:00:00.000',
-//     day : {
-//       icon: 5 ,
-//       iconPhrase: 'mock Phrase'
-//       },
-//     night: {
-//       icon: 20 ,
-//       iconPhrase: 'mock Phrase2'
-//       },
-//     maxTemper: 5,
-//     minTemper: -6,
-//     unit: 'C',
-//   },
-//   {
-//     date: '2020-04-11 15:00:00.000',
-//     day : {
-//       icon: 25 ,
-//       iconPhrase: 'mock Phrase3'
-//       },
-//     night: {
-//       icon: 31 ,
-//       iconPhrase: 'mock Phrase4'
-//       },
-//     maxTemper: 55,
-//     minTemper: -30,
-//     unit: 'C',
-//   },
-//   {
-//     date: '2022-04-10 15:00:00.000',
-//     day : {
-//       icon: 22 ,
-//       iconPhrase: 'mock Phrase5'
-//       },
-//     night: {
-//       icon: 30 ,
-//       iconPhrase: 'mock Phrase6'
-//       },
-//     maxTemper: 0,
-//     minTemper: 0,
-//     unit: 'C',
-//   },
-//   {
-//     date: '2022-11-10 15:00:00.000',
-//     day : {
-//       icon: 6 ,
-//       iconPhrase: 'mock Phrase7'
-//       },
-//     night: {
-//       icon: 14 ,
-//       iconPhrase: 'mock Phrase8'
-//       },
-//     maxTemper: 3,
-//     minTemper: -1,
-//     unit: 'C',
-//   },
-//   {
-//     date: '2022-12-10 15:00:00.000',
-//     day : {
-//       icon: 1 ,
-//       iconPhrase: 'mock Phrase9'
-//       },
-//     night: {
-//       icon: 34 ,
-//       iconPhrase: 'mock Phrase10'
-//       },
-//     maxTemper: -4,
-//     minTemper: -600,
-//     unit: 'C',
-//   }
-// ]
+export const mockWeatherForDay =
+[
+  {
+    date: '2020-07-10 15:00:00.000',
+    day : {
+      icon: 5 ,
+      iconPhrase: 'mock Phrase'
+      },
+    night: {
+      icon: 20 ,
+      iconPhrase: 'mock Phrase2'
+      },
+    maxTemper: 5,
+    minTemper: -6,
+    unit: 'C',
+  },
+  {
+    date: '2020-04-11 15:00:00.000',
+    day : {
+      icon: 25 ,
+      iconPhrase: 'mock Phrase3'
+      },
+    night: {
+      icon: 31 ,
+      iconPhrase: 'mock Phrase4'
+      },
+    maxTemper: 55,
+    minTemper: -30,
+    unit: 'C',
+  },
+  {
+    date: '2022-04-10 15:00:00.000',
+    day : {
+      icon: 22 ,
+      iconPhrase: 'mock Phrase5'
+      },
+    night: {
+      icon: 30 ,
+      iconPhrase: 'mock Phrase6'
+      },
+    maxTemper: 0,
+    minTemper: 0,
+    unit: 'C',
+  },
+  {
+    date: '2022-11-10 15:00:00.000',
+    day : {
+      icon: 6 ,
+      iconPhrase: 'mock Phrase7'
+      },
+    night: {
+      icon: 14 ,
+      iconPhrase: 'mock Phrase8'
+      },
+    maxTemper: 3,
+    minTemper: -1,
+    unit: 'C',
+  },
+  {
+    date: '2022-12-10 15:00:00.000',
+    day : {
+      icon: 1 ,
+      iconPhrase: 'mock Phrase9'
+      },
+    night: {
+      icon: 34 ,
+      iconPhrase: 'mock Phrase10'
+      },
+    maxTemper: -4,
+    minTemper: -600,
+    unit: 'C',
+  }
+]
 
