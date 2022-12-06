386624906149f63b231df33b18b18ee21131fdc3
DJUR-DJUR <djurodjur@gmail.com>
2022-12-07 00:06:05 +0200

    set dynamic Greeting message

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,11 +6,26 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
   styleUrls: ['./header.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
 
   @Input()
   public currentLocal!: string;
 
-  public dateNow = Date.now();
+  public dateNow = new Date;
+
+  public greeting!: string;
+
+  ngOnInit(): void {
+    const timeNow = this.dateNow.getHours();
+    if (timeNow >= 5 && timeNow < 11) {
+      this.greeting = 'Good Morning!!!';
+    } else if (timeNow >= 11 && timeNow < 17) {
+      this.greeting = 'Good Afternoon!!!';
+    } else if (timeNow >= 17 && timeNow < 23) {
+      this.greeting = 'Good Evening!!!';
+    } else if (timeNow >= 23 ||timeNow < 5) {
+      this.greeting = 'Good Night!!!';
+    };
+  }
 
 }
